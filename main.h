bdae35dbb48eb3e7a2b2eee4ae7eb28bec062b5d
ipacy02 <ipacy02@gmail.com>
2023-11-15 07:14:01 -0800

    push again the code

--- main.h
+++ main.h
@@ -4,7 +4,9 @@
 int _putchar(char c);
 int printf_ch(va_list val);
 int _printf(const char *format, ...);
-int printf_stringg(va_list val)
+int printf_stringg(va_list val);
+int _strlen(char *st);
+int _strlenc(const char *st)
 
 
 #endif
